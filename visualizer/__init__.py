2fb7f7ecd1196ee26e5f608fc6dc53b4965b090b
Han <hc13414@bristol.ac.uk>
2023-01-17 16:54:04 +0800

    Update __init__.py

--- visualizer/__init__.py
+++ visualizer/__init__.py
@@ -1,12 +1,7 @@
 from .vis_base import Visualizer_Base
 from .vis import Visualizer_AE
-<<<<<<< HEAD
-from .vis_2r import Visualizer_TwoR, Visualizer_TwoR_2D
-from .vis_raw import Visualizer_Raw, Visualizer_Raw_Pointcloud
-=======
 from .vis_2r import Visualizer_TwoR, Visualizer_TwoR_2D, Visualizer_TwoR_MAGG
 from .vis_raw import Visualizer_Raw
->>>>>>> 7b81da77357617057bbeb4931096b21da9697c8c
 
 # The following are here mostly for reference
 # from .vis_3D import Visualizer_Single_3D
